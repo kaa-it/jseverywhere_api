7df8ccafdca33d765639e8d8fce1efcdeb395ee5
Andrey Kruglov <kruglov.it@gmail.com>
2021-08-27 20:36:08 +0300

    crud

--- src/index.js
+++ src/index.js
@@ -1,2 +1,32 @@
-// index.js
-// This is the main entry point of our application
+require('dotenv').config();
+
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+
+const app = express();
+
+const port = process.env.PORT || 4000;
+const DB_HOST = process.env.DB_HOST;
+
+db.connect(DB_HOST);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return { models };
+  }
+});
+
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server listening at http://localhost:${port}${server.graphqlPath}`
+  )
+);
---
+++ src/resolvers/mutation.js
@@ -0,0 +1,33 @@
+module.exports = {
+  newNote: async (parent, { content }, { models }) => {
+    return await models.Note.create({
+      content,
+      author: 'Adam Scott'
+    });
+  },
+
+  updateNote: async (parent, { content, id }, { models }) => {
+    return await models.Note.findOneAndUpdate(
+      {
+        _id: id
+      },
+      {
+        $set: {
+          content
+        }
+      },
+      {
+        new: true
+      }
+    );
+  },
+
+  deleteNote: async (parent, { id }, { models }) => {
+    try {
+      await models.Note.findOneAndRemove({ _id: id });
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+};
